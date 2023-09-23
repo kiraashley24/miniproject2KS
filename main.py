c992ae3807d4acab23c1b1e829dbf29a7d745641
kiraashley24 <kiraselucky@gmail.com>
2023-09-23 17:21:49 -0600

    Finished subplot graphs for ranges

--- main.py
+++ main.py
@@ -13,33 +13,25 @@ import matplotlib.pyplot as plt
 #(10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 
 data = pd.read_csv('happiness.csv', index_col='Countries')
-
+seriesData = pd.Series(data["Happiness index, 2022"])
 
 #(10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 
 #Graph 1 showing top 5 happiest countries according to the index
 def topRankedGraph():
-    dataFive = data.head(5)
-    dataFive['Happiness index, 2022'].plot()
+    dataFive = seriesData.head(5)
+    dataFive.plot()
     plt.ylim(7.3, 7.9)
     plt.xlabel("Global rank", color='blue')
     plt.ylabel("Happiness Index", color= 'green')
     plt.title("Countries Ranked by Happiness")
     plt.show()
 
-def getList():
-    happyList = []
-
-    for index in data["Happiness index, 2022"]:
-        happyList.append(index)
-
-    return happyList
-
 def statsGraph():
-    meanValue = data["Happiness index, 2022"].mean()
-    medianValue = data["Happiness index, 2022"].median()
-    minValue = data["Happiness index, 2022"].min()
-    maxValue = data["Happiness index, 2022"].max()
+    meanValue = seriesData.mean()
+    medianValue = seriesData.median()
+    minValue = seriesData.min()
+    maxValue = seriesData.max()
     plt.bar(['Mean', 'Median', 'Minimum', 'Maximum'], [meanValue, medianValue, minValue, maxValue])
     plt.ylim(1, 8)
     plt.xlabel("Stat Values")
@@ -47,20 +39,34 @@ def statsGraph():
     plt.title("Statistics of Global Happiness Index")
     plt.show()
 
-#def rangeGraph():
-   # graphList = getList()
+def rangeGraph():
+    lowerRange = seriesData[seriesData < 5]
+    higherRange = seriesData[seriesData >= 5]
 
+    # Create a figure with two subplots
+    fig, axes = plt.subplots(2)
+    fig.suptitle('Number of Countries Range Subplots')
+    axes[0].plot(higherRange, color='green')
+    axes[0].set_title("Happiness Index of 5 or greater", color='green')
+    axes[0].set_xticklabels([])
+    axes[0].legend([len(higherRange)])
+    axes[1].plot(lowerRange, color='red')
+    axes[1].set_title("Happiness Index of less than 5", color='red')
+    axes[1].set_xticklabels([])
+    axes[1].legend([len(lowerRange)])
 
+    # Show the subplots
+    plt.show()
 
-    #axs = data[['Happiness index, 2022'] > 5].plot.area(figsize=(12,4), subplots=True)
-    #plt.show()
 #(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 
 
 
 #Start of program
-
+topRankedGraph()
 statsGraph()
+rangeGraph()
+
 
 
 #(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
